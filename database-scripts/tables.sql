5c3450ccf368c26661fb478d40263adf9dca8649
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-10-01 21:53:31 +0200

    feat: model docs comments

--- database-scripts/tables.sql
+++ database-scripts/tables.sql
@@ -1,7 +1,7 @@
 CREATE TABLE BlogPost (
      id SERIAL PRIMARY KEY,
      text VARCHAR(2000) NOT NULL,
-     username VARCHAR(256) NOT NULL,
+     username VARCHAR(128) NOT NULL,
      dateOfPublication Date NOT NULL,
      avatar VARCHAR(512),
      image VARCHAR(512)
--- server/src/models/blogpost.rs
+++ server/src/models/blogpost.rs
@@ -7,10 +7,23 @@ use crate::schema::blogpost::blogpost;
 #[diesel(table_name = blogpost)]
 pub struct BlogPost {
     pub id: i32,
+
+    /// not longer than 2000
     pub text: String,
+
+    /// not longer than 128
     pub username: String,
+
     #[diesel(column_name = dateOfPublication)]
     pub date_of_publication: NaiveDate,
+
+    /// if present, it represents the path to the image in local storage
+    /// otherwise placeholder will be used
+    /// not longer than 512
     pub image: Option<String>,
+
+    /// if present, it represents the path to the image in local storage
+    /// otherwise placeholder will be used
+    /// not longer than 512
     pub avatar: Option<String>
 }

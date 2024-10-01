6eff27946201e15c60b2bee4965c4d037b1cff0e
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-10-01 18:49:39 +0200

    feat: init

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+
+// TODO: pass port as an env var
+
+use actix_web::{App, HttpServer};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+    })
+    .bind(("127.0.0.1", 8085))?
+    .run()
+    .await
+}

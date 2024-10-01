f1a9a267d35e19f4202cf4690a9d92da32fc51d5
Dragoslav Tamindžija <dragodev@protonmail.com>
2024-10-01 18:55:08 +0200

    feat: logger

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+use actix_web::{middleware::Logger, App, HttpServer};
+use env_logger::Env;
+
+const LOGGER_FORMAT: &str = "[%t] %a %s UA:%{User-Agent}i CT:%{Content-Type}i %Dms";
 
 // TODO: pass port as an env var
 
-use actix_web::{App, HttpServer};
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+
     HttpServer::new(|| {
         App::new()
+            .wrap(Logger::new(LOGGER_FORMAT))
     })
     .bind(("127.0.0.1", 8085))?
     .run()
